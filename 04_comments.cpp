2a23ef967826d18be125953736496d47f1ac5ab0
sravanthichalla8374-png <sravanthichalla8374@gmail.com>
2026-09-11 13:29:50 +0530

    Create 04_comments.cpp

---
+++ 04_comments.cpp
@@ -0,0 +1,8 @@
+/*printing.cpp program for simple display*/
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"welcome to microchip computer Education";
+    // it prints the text on the screen
+    return 0;
+}
