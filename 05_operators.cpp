5de07be5171dc17efd1dd855c20b59e53d595eda
sravanthichalla8374-png <sravanthichalla8374@gmail.com>
2026-09-15 13:08:45 +0530

    Create 05_operators.cpp

---
+++ 05_operators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=6,b=8;
+    cout<<a*b<<endl;
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<a/b<<endl;
+    cout<<a%b<<endl;
+    return 0;
+}
